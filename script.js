7d8f80d7932f9cd3756fb02d222580ca2c97fb7a
Steven Carton <spatc@protonmail.com>
2021-03-01 15:37:57 -0500

    converting object to maps and defining maps

--- script.js
+++ script.js
@@ -64,36 +64,54 @@ const restaurant = {
 };
 
 // maps
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-console.log(rest.set(2, 'Lisbon, Portugal'));
 
-rest
-  .set('catgories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open :D')
-  .set(false, 'We are closed :(');
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// another way to populate maps without set method
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'Javascript'],
+  [('correct', 3)],
+  [(true, 'Correct🍕')],
+  [false, 'Try Again!'],
+]);
+console.log(question);
 
-const time = 21;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// convert object to map
+console.log(Object.entries(hours));
+const hoursMap = new Map(Object.entries(hours));
+console.log(hoursMap);
 
-// map methods
-console.log(rest.has('categories'));
-rest.delete(2);
-console.log(rest.size);
-// rest.clear()
-console.log(rest.size);
-const arr = [1, 2];
-rest.set(arr, 'Test');
-console.log(rest);
-console.log(rest.size);
-// [1,2] are not the same objects as each other
-console.log(rest.get(arr));
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+
+// rest
+//   .set('catgories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed :(');
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// // map methods
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// console.log(rest.size);
+// // rest.clear()
+// console.log(rest.size);
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// console.log(rest);
+// console.log(rest.size);
+// // [1,2] are not the same objects as each other
+// console.log(rest.get(arr));
 // sets
 // const ordersSet = new Set([
 //   'Pasta',
