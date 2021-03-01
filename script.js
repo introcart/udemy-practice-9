9f6ea1f68ca76b1df091d99ba4e27d162b979ced
Steven Carton <spatc@protonmail.com>
2021-03-01 01:43:42 -0500

    working with maps, not finished yet

--- script.js
+++ script.js
@@ -63,6 +63,37 @@ const restaurant = {
   },
 };
 
+// maps
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('catgories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// map methods
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest.size);
+// rest.clear()
+console.log(rest.size);
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest);
+console.log(rest.size);
+// [1,2] are not the same objects as each other
+console.log(rest.get(arr));
 // sets
 // const ordersSet = new Set([
 //   'Pasta',
