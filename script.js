615e18052c873de83f7d22da97851e75c98ce467
Steven Carton <spatc@protonmail.com>
2021-03-02 00:07:12 -0500

    just finished 4th coding challenge of the section, successfully

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -63,49 +64,73 @@ const restaurant = {
   },
 };
 
+// Challenge 4
+const text = document.querySelector('textarea');
+const btn = document.querySelector('button');
+
+const generateChecks = function (n) {
+  return '✅'.repeat(n);
+};
+
+btn.addEventListener('click', function () {
+  const textInput = text.value;
+  const textArray = textInput.split('\n');
+  let newArray = [];
+  let count = 0;
+  for (let word of textArray) {
+    let newWord = word.trim().toLowerCase();
+    let idx = newWord.indexOf('_');
+    count += 1;
+    let newWord1 =
+      newWord.slice(0, idx) +
+      newWord[idx + 1].toUpperCase() +
+      newWord.slice(idx + 2);
+    console.log(newWord1.padEnd(20, ' ') + generateChecks(count));
+  }
+});
 // Working with strings part 3
 
-console.log('a+very+nice+string'.split('+'));
-const [firstName, lastName] = 'Steven Carton'.split(' ');
+// console.log('a+very+nice+string'.split('+'));
+// const [firstName, lastName] = 'Steven Carton'.split(' ');
 
-const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
-console.log(newName);
+// const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+// console.log(newName);
 
-const capitalizeName = function (name) {
-  const names = name.split(' ');
-  const namesUpper = [];
-  for (const n of names) {
-    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
-    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
-  }
-  console.log(namesUpper.join(' '));
-};
-capitalizeName('steven patrick carton');
+// const capitalizeName = function (name) {
+//   const names = name.split(' ');
+//   const namesUpper = [];
+//   for (const n of names) {
+//     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+//   console.log(namesUpper.join(' '));
+// };
+// capitalizeName('steven patrick carton');
 
-// Padding
-const message = 'Go to gate 23!';
-console.log(message.padStart(25, '+').padEnd(35, '+'));
-console.log('Jonas'.padStart(25, '+'));
+// // Padding
+// const message = 'Go to gate 23!';
+// console.log(message.padStart(25, '+').padEnd(35, '+'));
+// console.log('Jonas'.padStart(25, '+'));
 
-const maskedCreditCard = function (number) {
-  const str = number + '';
-  const last = str.slice(-4);
-  return last.padStart(str.length, '*');
-};
+// const maskedCreditCard = function (number) {
+//   const str = number + '';
+//   const last = str.slice(-4);
+//   return last.padStart(str.length, '*');
+// };
 
-console.log(maskedCreditCard(445664637836));
+// console.log(maskedCreditCard(445664637836));
 
-// repeat method
-const message2 = 'Bad weather... All Departures Delayed... ';
-console.log(message2.repeat(5));
+// // repeat method
+// const message2 = 'Bad weather... All Departures Delayed... ';
+// console.log(message2.repeat(5));
 
-const planesInLine = function (n) {
-  console.log(`There are ${n} planes in line ${'🛩 '.repeat(n)}`);
-};
+// const planesInLine = function (n) {
+//   console.log(`There are ${n} planes in line ${'🛩 '.repeat(n)}`);
+// };
 
-planesInLine(5);
-planesInLine(3);
-planesInLine(12);
+// planesInLine(5);
+// planesInLine(3);
+// planesInLine(12);
 // Working with strings part 2
 // const airline = 'TAP Air Portugal';
 
